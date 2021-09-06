8a024f0372b23e33c9b7e454ab183f485d1a0e1f
Serhat Caner <srhtcnr93@gmail.com>
2021-09-06 17:19:35 +0300

    First commit

---
+++ src/main/java/com/serhatcaner/notethistime/NotethistimeApplication.java
@@ -0,0 +1,13 @@
+package com.serhatcaner.notethistime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotethistimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotethistimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serhatcaner/notethistime/NotethistimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatcaner.notethistime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotethistimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
